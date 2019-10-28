68b686930b949967e90616f7958dd1bfd2358df0
Chip Zoller <chipzoller@gmail.com>
2019-10-28 10:33:05 -0400

    Update pks-rancher-reg.sh
    Removing spaces around operator that validates variables (`sh: =~: unknown operand`)

--- pks-rancher-reg.sh
+++ pks-rancher-reg.sh
@@ -8,7 +8,7 @@ echo "Rancher server is: $RANCHER_SERVER"
 #echo "Rancher cert is: $(cat $CURL_CA_BUNDLE)"
 # Register the cluster with Rancher returning the cluster ID value. Fail if value is empty.
 RANCHER_CID=$(curl -s -H "Authorization: Bearer $RANCHER_BEARER" -H "Accept: application/json" -H "Content-Type: application/json" -X POST "https://$RANCHER_SERVER/v3/cluster" -d '{ "dockerRootDir": "/var/vcap/store/docker/docker", "enableClusterAlerting": false, "enableClusterMonitoring": false, "enableNetworkPolicy": false, "type": "cluster", "name": "'"$PKS_CLUSTER"'" }' | jq -r '.id')
-if [[ ! $RANCHER_CID =~ ^c- ]];
+if [[ ! $RANCHER_CID=~^c- ]];
 then
   echo "Returned cluster ID is not in the expected format. Check to ensure the CA certificate contains line breaks before and after the contents."
   exit 1
@@ -17,7 +17,7 @@ else
 fi
 # Call to Rancher to generate the registration manifest. Fail if value is empty.
 RANCHER_REG=$(curl -s -H "Authorization: Bearer $RANCHER_BEARER" -H "Accept: application/json" -H "Content-Type: application/json" -X POST "https://$RANCHER_SERVER/v3/clusterregistrationtoken" -d '{"type":"clusterRegistrationToken","clusterId":"'"$RANCHER_CID"'"}' | jq -r '.manifestUrl')
-if [[ ! $RANCHER_REG =~ ^https.*yaml$ ]];
+if [[ ! $RANCHER_REG=~^https.*yaml$ ]];
 then
   echo "RANCHER_REG did not return a valid path to YAML. Script will fail."
   exit 1
